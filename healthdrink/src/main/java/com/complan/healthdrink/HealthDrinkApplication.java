5a57fc376bbe20ad54479da8d595288ef592d76e
ramprasathram <42640856+ramprasathram@users.noreply.github.com>
2021-01-25 12:56:38 +0530

    Soap Service in Gradle Configuration
    Soap Service in Gradle Configuration

---
+++ healthdrink/src/main/java/com/complan/healthdrink/HealthDrinkApplication.java
@@ -0,0 +1,13 @@
+package com.complan.healthdrink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthDrinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthDrinkApplication.class, args);
+	}
+
+}
---
+++ healthdrink/src/test/java/com/complan/healthdrink/HealthDrinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complan.healthdrink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthDrinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
